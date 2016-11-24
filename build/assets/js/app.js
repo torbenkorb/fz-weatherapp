8ca75031550894c427ba6d9cec7b27602d2d9c4c
Torben Korb <korb@a-w-media.de>
2016-11-24 04:31:33 +0100

    add the functionality to add locations to the existing cities object, add items to localStorage

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -10,11 +10,22 @@
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
   ])
-    .controller('ForecastApi', function($scope, $http, $location) {
+    .controller('WeatherAppController', function($scope, $http) {
 
       $scope.Math = window.Math;
-      $scope.location = $location;
       $scope.defaultCity = 'Frankfurt am Main';
+      $scope.currentLocation = {};
+
+      $scope.cities = {
+
+        "frankfurt am main" : { coords: { latitude: 50.1109221, longitude: 8.682126700000026 } },
+        "berlin" : { coords: { latitude: 52.52000659999999, longitude: 13.404953999999975 } },
+        "london" : { coords: { latitude: 51.5073509, longitude: -0.12775829999998223 } },
+        "new york" : { coords: { latitude: 40.7127837, longitude: -74.00594130000002 } },
+        "los angeles" : { coords: { latitude: 34.0522342, longitude: -118.2436849 } },
+        "tokyo" : { coords: { latitude: 35.7090259, longitude: 139.73199249999993 } }
+
+      };
 
       $scope.geocodeLatLng = function(geocoder, latlng) {
         geocoder.geocode({
@@ -23,18 +34,14 @@
         }, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
             if (results) {
-              console.log(results);
               var result = results[0].address_components;
-                var info = '';
+                var cityName = '';
                 for(var i=0; i<result.length; ++i) {
                     if(result[i].types[0]=="locality") {
-                      info = result[i].long_name;
-                      $scope.city = info;
+                      cityName = result[i].long_name;
+                      $scope.city = cityName;
                     }
                 }
-                console.log(info);
-
-
             } else {
               window.alert('No results found');
             }
@@ -47,21 +54,29 @@
       $scope.loadWeather = function(cityCoords) {
 
         var latlng = cityCoords.coords.latitude + "," + cityCoords.coords.longitude;
-        var forecastURL = 'https://api.darksky.net/forecast/9c5f115423c6a7bdf61901d449355c00/' + latlng + '?units=si&callback=JSON_CALLBACK';
+        var apiURL = 'https://api.darksky.net/forecast/9c5f115423c6a7bdf61901d449355c00/' + latlng + '?units=si&callback=JSON_CALLBACK';
         var gMapsLatLngStr = latlng.split(',', 2);
         var gMapsLatLng = {lat: parseFloat(gMapsLatLngStr[0]), lng: parseFloat(gMapsLatLngStr[1])};
 
         var geocoder = new google.maps.Geocoder();
         $scope.geocodeLatLng(geocoder, gMapsLatLng);
 
-        $http.jsonp(forecastURL).then(function(res){
+        $http.jsonp(apiURL).then(function(res){
+          // Successfully retrieved data from the DarkSky API
           $scope.json = res.data;
           $scope.icon = icons[$scope.json.currently.icon];
           $scope.icon1 = icons[$scope.json.daily.data[1].icon];
           $scope.icon2 = icons[$scope.json.daily.data[2].icon];
           $scope.icon3 = icons[$scope.json.daily.data[3].icon];
+        }, function() {
+          // Error retrieving data from the DarkSky API
+          console.log('There was an error retrieving data from the DarkSky API');
         }).finally(function() {
           $scope.loading = false;
+          $scope.currentLocation = {
+            name: $scope.city,
+            coords: cityCoords.coords
+          };
         });
 
       };
@@ -76,7 +91,7 @@
             $scope.loadDefaultCity();
           }
         } else {
-          $scope.loadWeather(cities[city.toLowerCase()]);
+          $scope.loadWeather($scope.cities[city.toLowerCase()]);
         }
       };
 
@@ -93,8 +108,7 @@
       var degrees = 360;
 
       $scope.reloadCity = function(e) {
-        console.log($scope.city);
-        if(!($scope.city.toLowerCase() in cities)) {
+        if(!($scope.city.toLowerCase() in $scope.cities)) {
           $scope.loadCity('Current Location');
         } else {
           $scope.loadCity($scope.city);
@@ -104,6 +118,40 @@
         // angular.element(e.currentTarget).find('svg').css('-webkit-transform', 'rotate(' + degrees + 'deg)');
         // degrees += 360;
       };
+
+      $scope.saveLocation = function() {
+        console.log('Save location!');
+        console.log($scope.currentLocation);
+        $scope.cities[$scope.currentLocation.name.toLowerCase()] = {
+          coords: {
+            latitude: $scope.currentLocation.coords.latitude,
+            longitude: $scope.currentLocation.coords.longitude
+          }
+        };
+        storeData();
+        console.log($scope.cities);
+      };
+
+
+      function storeData() {
+        if (storageAvailable('localStorage')) {
+          // Yippee! We can use localStorage awesomeness
+          if(!localStorage.getItem('berlin')) {
+            // Populate Storage if no data available
+            angular.forEach($scope.cities, function(value, key) {
+              console.log(key, value);
+              localStorage.setItem(key, value.coords.latitude + ',' + value.coords.longitude);
+            });
+          } else {
+            // Read the existing data
+            console.log('Rebuild the cities object in scope from the local storage');
+          }
+        }
+      }
+      storeData();
+
+
+
 
 
       var refreshGesture = new Hammer(document.getElementById('mainApp'));
@@ -163,17 +211,19 @@
 
   };
 
-  var cities = {
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+        x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
 
-    "frankfurt am main" : { coords: { latitude: 50.1109221, longitude: 8.682126700000026 } },
-    "berlin" : { coords: { latitude: 52.52000659999999, longitude: 13.404953999999975 } },
-    "london" : { coords: { latitude: 51.5073509, longitude: -0.12775829999998223 } },
-    "new york" : { coords: { latitude: 40.7127837, longitude: -74.00594130000002 } },
-    "los angeles" : { coords: { latitude: 34.0522342, longitude: -118.2436849 } },
-    "tokyo" : { coords: { latitude: 35.7090259, longitude: 139.73199249999993 } },
-    "current location" : {}
-
-  };
 
 })();
 
